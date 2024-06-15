35398251c113a2e42ca9f6fe72f32156a114040c
hugolopespinto <72020673+hugolopespinto@users.noreply.github.com>
2024-06-15 13:26:39 +0200

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import Navbar from './navbar';
+
+function App() {
+  const [showAddForm, setShowAddForm] = useState(false);
+  const [items, setItems] = useState([]);
+  const [newItemName, setNewItemName] = useState('');
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = () => {
+    axios.get('http://localhost:3001/elements')
+      .then(response => {
+        setItems(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the items!', error);
+      });
+  };
+
+  const handleAddItem = () => {
+    axios.post('http://localhost:3001/elements', { name: newItemName })
+      .then(response => {
+        console.log('Item added successfully:', response.data);
+        setNewItemName('');
+        setShowAddForm(false);
+        // After adding item, fetch updated items list
+        fetchItems();
+      })
+      .catch(error => {
+        console.error('Error adding item:', error);
+      });
+  };
+
+  const handleClearList = () => {
+    axios.delete('http://localhost:3001/clear-items')
+      .then(response => {
+        console.log('Items cleared successfully:', response.data);
+        // After clearing items, fetch updated items list
+        fetchItems();
+      })
+      .catch(error => {
+        console.error('Error clearing items:', error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <Navbar /> {/* Utiliser le composant Navbar */}
+      <div className="button-container">
+        <button onClick={() => setShowAddForm(true)}>Add a new car</button>
+        <button onClick={handleClearList}>Clear the list</button>
+      </div>
+
+      {showAddForm && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setShowAddForm(false)}>&times;</span>
+            <h2>Add a new car to the list</h2>
+            <input
+              type="text"
+              value={newItemName}
+              onChange={(e) => setNewItemName(e.target.value)}
+              placeholder="Enter car name / model"
+            />
+            <button onClick={handleAddItem} disabled={newItemName.trim() === ''}>ADD</button>
+          </div>
+        </div>
+      )}
+
+      <div className="items-list">
+        <h2 style={{ textAlign: 'center' }}>Items List</h2>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <div style={{ maxWidth: '400px', textAlign: 'left' }}>
+            {items.map((item, index) => (
+              <div key={index} style={{ marginBottom: '10px' }}>{item.name}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/navbar.js
@@ -0,0 +1,13 @@
+// src/Navbar.js
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>List of Cars I Want</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
